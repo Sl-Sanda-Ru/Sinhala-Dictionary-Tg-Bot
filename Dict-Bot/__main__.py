ed3a392887731b2a34b931fa3d5c811955ae0acc
Sandaru Ashen <sandaruashen02@gmail.com>
2023-04-13 20:23:27 +0530

    bug fix

--- Dict-Bot/__main__.py
+++ Dict-Bot/__main__.py
@@ -16,9 +16,9 @@ def chunker(seq, size):
 
 bot = Client(
     "En-To-Si-Bot",
-    bot_token=environ['BOT_TOKEN'],
-    api_hash=environ['API_HASH'],
-    api_id=environ['API_ID']
+    bot_token = 'YOUR BOT TOKEN, OBTAIN IT FROM @BotFather',
+    api_hash = 'YOUR API HASH, OBTAIN IT FROM https://my.telegram.org/auth',
+    api_id = 
     )
 @bot.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
@@ -26,10 +26,10 @@ async def start(client, message):
     reply_markup=InlineKeyboardMarkup(WELCOME_KEY))
 @bot.on_message(filters.private & filters.text)
 async def trans(client, message):
-    if len(message.text.strip().split()) > 1:
-        await  message.reply_text('✅ ' + gtrans(message.text.strip() + '\nBot By :\t@Sandaru_Ashen'),reply_to_message_id = message.id)
+    if len(message.text.strip().split()) > 1 or not(message.text.strip()).isalnum():
+        await  message.reply_text('✅ ' + gtrans(message.text.strip() + '\nBot By :\t@sandaru_ashen'),reply_to_message_id = message.id)
     elif join_search(CONN,message.text.lower())[0] is True:
-        await message.reply_text(text ='✅ ' + '\n✅ '.join(join_search(CONN,message.text.lower())[1]) + '\nBot By :\t@Sandaru_Ashen',reply_to_message_id = message.id)
+        await message.reply_text(text ='✅ ' + '\n✅ '.join(join_search(CONN,message.text.lower())[1]) + '\nBot By :\t@sandaru_ashen',reply_to_message_id = message.id)
     elif join_search(CONN,message.text.lower())[0] is False:
         keyboard = []
         for i in chunker(join_search(CONN,message.text)[1],2):
@@ -49,7 +49,7 @@ async def trans(client, message):
 @bot.on_callback_query()
 async def callback(client, update):
     if join_search(CONN,update.data)[1][0] != '':
-        await update.message.edit('✅ ' + '\n✅ '.join(join_search(CONN,update.data)[1]) + '\nBot By :\t@Sandaru_Ashen')
+        await update.message.edit('✅ ' + '\n✅ '.join(join_search(CONN,update.data)[1]) + '\nBot By :\t@sandaru_ashen')
     else:
         await update.message.edit('✅ ' + gtrans(update.data) + '\nBot By :\t@sandaru_ashen')
 if __name__ == '__main__':
